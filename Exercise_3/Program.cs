f911079362c1a50cd84183cf26e0a1a76d84e467
Ghani Zumar <ghani.zumar.ft21@mail.umy.ac.id>
2022-11-21 16:03:01 +0700

    add method firstNode

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -63,6 +63,14 @@ namespace Exercise_3
                 Console.Write(LAST.rollNumber + "    " + LAST.name + "\n");
             }
         }
+        public void firstNode()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+                Console.WriteLine("\nThe first record in the list is:\n\n" +
+                    LAST.next.rollNumber + "   " + LAST.next.name);
+        }
     }
     
     class program
